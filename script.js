a1130362726d1f19df5e5cf7ca5b1ef85319c1e2
JZanataNeto <cartasdojoao@gmail.com>
2024-12-06 16:03:50 -0300

    adicionando sons de play, pause e beep

--- script.js
+++ script.js
@@ -4,20 +4,47 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+const musicaFocoinput = document.querySelector('#alternar-musica');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const alternarImagem = document.querySelector('.app__card-primary-butto-icon');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const musicaBeep = new Audio('/sons/beep.mp3');
+const musicaPause = new Audio('/sons/pause.mp3');
+const musicaPlay = new Audio('/sons/play.wav');
+
+let intervaloId = null;
+let tempoDecorridoEmSegundos = 5;
+musica.loop = true;
+
+musicaFocoinput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+});
 
 focoBt.addEventListener('click', () => {
     alterarContexto('foco');
+    focoBt.classList.add('active');
 });
 
 curtoBt.addEventListener('click', () => {
     alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
 });
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active');
+    })
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
     switch (contexto) {
@@ -35,4 +62,38 @@ function alterarContexto(contexto) {
         default:
             break;
     }
+}
+
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        //musicaBeep.play();
+        alert('Tempo finalizado');
+        zerar();
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+    console.log(`Temporizador: `+ tempoDecorridoEmSegundos);
+    console.log(`Id: ` + intervaloId);
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if(intervaloId) {
+        musicaPause.play();
+        zerar();
+        return;
+    }
+    musicaPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000);
+    alternarImagem.setAttribute('src', '/imagens/pause.png');
+    iniciarOuPausarBt.textContent = "Pausar";
+}
+
+function zerar() {
+    clearInterval(intervaloId);
+    alternarImagem.setAttribute('src', '/imagens/play_arrow.png');
+    iniciarOuPausarBt.textContent = "Começar";
+    intervaloId = null;
 }
